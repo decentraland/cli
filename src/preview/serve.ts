4260eafd93e6cb4dd099de3bb6cc61fb2584a1fb
menduz <agustin.mz.92@gmail.com>
2018-03-13 17:29:12 -0300

    chore: remove bundled files

--- src/preview/serve.ts
+++ src/preview/serve.ts
@@ -6,35 +6,31 @@ import * as express from 'express';
 import { getRoot } from '../utils/get-root';
 import { cliPath } from '../utils/cli-path';
 
-export function serve(vorpal: any, args: any, ): any {
+export function serve(vorpal: any, args: any): any {
   vorpal.log(chalk.blue('Parcel server is starting...\n'));
 
   const root = getRoot();
-  const reloadify = require('reloadify')(root);
 
   const app = express();
 
-  app.get('/parcelInfo.json', (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(`{"parcels": {"0,0": "0,,,/"}}`);
-  });
   app.get('/', (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(`<!DOCTYPE html>
-  <html>
+    res.end(`
+      <!DOCTYPE html>
+      <html>
         <head>
-              <title>Decentraland Preview</title>
-              <script charset="utf-8" src="/static/preview.js"></script>
-              <script charset="utf-8" src="/static/parcel-boundary.js"></script>
-            </head>
+          <title>Decentraland Preview</title>
+          <script charset="utf-8" src="/dcl-sdk/preview.js"></script>
+        </head>
         <body>
         </body>
-  </html>`
-    );
+      </html>
+    `);
   });
-  app.use('/static', express.static(path.join(cliPath, 'static')));
+
+  // serve the folder `dcl-sdk/artifacts` as `/dcl-sdk`
+  app.use('/dcl-sdk', express.static(path.dirname(require.resolve('dcl-sdk/artifacts/preview'))));
   app.use(express.static(root));
-  app.use(reloadify);
   app.listen(2044, '0.0.0.0');
   return app;
 }
