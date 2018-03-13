557d140ec29c7e9772136b22ef840df8ebd5de4f
Esteban Ordano <esteban@decentraland.org>
2018-03-13 14:09:13 -0300

    fix: segmentio users

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -14,6 +14,7 @@ import { isDev } from './is-dev';
 ///
 
 const WRITE_KEY = 'sFdziRVDJo0taOnGzTZwafEL9nLIANZ3';
+const SINGLE_USER = 'cli-user';
 export const analytics = new Analytics(WRITE_KEY);
 
 ///
@@ -30,8 +31,9 @@ export const deploy = track('Scene deploy requested');
 export async function postInstall() {
   const userId = await getUserId();
   analytics.identify({
-    userId,
+    userId: SINGLE_USER,
     traits: {
+      userId,
       os: process.platform,
       createdAt: new Date().getTime()
     }
@@ -50,8 +52,11 @@ export function track(eventName: string) {
     }
     const userId = await getUserId();
     analytics.track({
-      userId,
+      userId: SINGLE_USER,
       event: eventName,
+      properties: {
+        userId
+      }
     });
   };
 }
