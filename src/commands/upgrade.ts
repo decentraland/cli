f0b1102443cf13cdbe33cf42e4136b6816985ca3
Esteban Ordano <esteban@decentraland.org>
2018-03-15 23:15:40 -0300

    fix: npm on windows

--- src/commands/upgrade.ts
+++ src/commands/upgrade.ts
@@ -1,12 +1,13 @@
 import { wrapAsync } from '../utils/wrap-async';
 import { spawn } from 'child_process';
+import { npm } from '../utils/module-helpers';
 
 export function upgrade(vorpal: any) {
   vorpal
     .command('upgrade')
     .description('Update the Decentraland CLI tools')
     .action(function(args: any, callback: () => void) {
-      const child = spawn('npm', ['install', '-g', '--upgrade', 'decentraland']);
+      const child = spawn(npm, ['install', '-g', '--upgrade', 'decentraland']);
       child.stdout.pipe(process.stdout);
       child.stderr.pipe(process.stderr);
       child.on('close', callback);
