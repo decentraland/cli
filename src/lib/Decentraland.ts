2e6628329f70968200f14b651edeb8f8dee326f9
belohlavek <dani@decentraland.org>
2018-04-11 14:50:29 -0300

    chore: removed unused dependencies

--- src/lib/Decentraland.ts
+++ src/lib/Decentraland.ts
@@ -5,7 +5,6 @@ import { Ethereum } from './Ethereum'
 import * as events from 'wildcards'
 import { getRootPath } from '../utils/project'
 import { LinkerAPI } from './LinkerAPI'
-import { buildTypescript } from '../utils/moduleHelpers'
 import { Preview } from './Preview'
 
 export interface IDecentralandArguments {
