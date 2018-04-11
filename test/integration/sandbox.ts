51cbd9bc1814474d96c02450c12626cd30a1ab33
belohlavek <dani@decentraland.org>
2018-04-11 15:12:25 -0300

    fix: await

--- test/integration/sandbox.ts
+++ test/integration/sandbox.ts
@@ -1,11 +1,12 @@
 import * as fs from 'fs-extra'
 import * as path from 'path'
-const rimraf = require('rimraf')
+import * as rimraf from 'rimraf'
 
 export function tmpTest(fn: any) {
   return new Promise(async (resolve, reject) => {
     const name = 'test-' + (+Date.now()).toString()
     const dir = path.resolve(process.cwd(), name)
+
     const folder = await fs.mkdir(dir)
 
     await fn(dir)
