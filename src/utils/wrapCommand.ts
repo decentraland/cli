55809ad407cb560c0dc9ab4ef5284e63d505b7c4
Daniel Belohlavek <dbhvk.96@gmail.com>
2018-04-23 17:42:10 -0300

    fix: outdated warnings should appear in new line (#146)

--- src/utils/wrapCommand.ts
+++ src/utils/wrapCommand.ts
@@ -6,23 +6,23 @@ export function wrapCommand(fn: (args: any, callback: () => void) => Promise<any
   return function(args, cb) {
     isMetaverseApiOutdated().then(isOutdated => {
       if (isOutdated) {
-        this.log(highlight('WARNING: outdated metaverse-api version\nPlease run ') + 'npm install metaverse-api@latest')
+        this.log(highlight('\nWARNING: outdated metaverse-api version\nPlease run ') + 'npm install metaverse-api@latest\n')
       }
-    })
 
-    isCLIOutdated().then(isOutdated => {
-      if (isOutdated) {
-        this.log(highlight('WARNING: outdated decentraland version\nPlease run ') + 'npm update -g decentraland')
-      }
-    })
+      isCLIOutdated().then(isOutdated => {
+        if (isOutdated) {
+          this.log(highlight('\nWARNING: outdated decentraland version\nPlease run ') + 'npm update -g decentraland\n')
+        }
 
-    fn
-      .call(this, args, cb)
-      .then(val => cb())
-      .catch((e: Error) => {
-        Analytics.reportError(e.name, e.message, e.stack).then(() => {
-          exit(e.message, this)
-        })
+        fn
+          .call(this, args, cb)
+          .then(val => cb())
+          .catch((e: Error) => {
+            Analytics.reportError(e.name, e.message, e.stack).then(() => {
+              exit(e.message, this)
+            })
+          })
       })
+    })
   }
 }
