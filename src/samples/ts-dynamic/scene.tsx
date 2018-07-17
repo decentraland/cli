432d9bcacb4575d66994f1efcca80fcca7ed901d
Federico Miras <miras.federico@gmail.com>
2018-07-17 13:58:05 -0300

    fix: rollback to door rotation sample on ts-dynamic

--- src/samples/ts-dynamic/scene.tsx
+++ src/samples/ts-dynamic/scene.tsx
@@ -1,50 +1,37 @@
 import { createElement, ScriptableScene } from 'metaverse-api'
 
-const networkHz = 6
-const interval = 1000 / networkHz
+// This is an interface, you can use it to enforce the types of your state
+export interface IState {
+  isDoorClosed: boolean
+}
 
-export default class RollerCoaster extends ScriptableScene<any, { time: number }> {
-  state = { time: 0 }
+export default class HouseScene extends ScriptableScene<any, IState> {
+  // This is your initial state and it respects the given IState interface
+  state = {
+    isDoorClosed: false
+  }
 
-  timeout = setInterval(() => {
-    this.setState({
-      time: performance.now() * 0.0001
+  sceneDidMount() {
+    this.eventSubscriber.on('door_click', () => {
+      // setState() will update the state and trigger an update, causing the scene to rerender
+      this.setState({ isDoorClosed: !this.state.isDoorClosed })
     })
-  }, interval)
-
-  sceneWillUnmount() {
-    clearInterval(this.timeout)
   }
 
   async render() {
-    const { time } = this.state
-
-    const size = 2
-
-    const x = Math.cos(time) * Math.cos(time) * size
-    const y = Math.cos(time) * Math.sin(time) * size
-    const z = Math.sin(time) * size
+    const doorRotation = {
+      x: 0,
+      y: this.state.isDoorClosed ? 0 : 90,
+      z: 0
+    }
 
     return (
-      <scene>
-        <entity position={{ x: 5, y: 4, z: 5 }}>
-          <entity
-            id="train"
-            position={{ x, y, z }}
-            rotation={{ x: Math.cos(time) * 40, y: Math.sin(time) * 40, z: 0 }}
-            transition={{
-              position: { duration: interval },
-              rotation: { duration: interval }
-            }}
-          >
-            <box position={{ x: 0, y: -1, z: 0 }} color="#000000" scale={{ x: 3, y: 0.4, z: 5 }} />
-            <box position={{ x: 1.5, y: 0, z: 0 }} color="#FF0000" scale={{ x: 0.2, y: 1, z: 5 }} />
-            <box position={{ x: -1.5, y: 0, z: 0 }} color="#FFFF00" scale={{ x: 0.2, y: 1, z: 5 }} />
-
-            <box position={{ x: 0, y: 0, z: 2.5 }} color="#00FF00" scale={{ x: 3, y: 1, z: 0.2 }} />
-            <box position={{ x: 0, y: 0, z: -2.5 }} color="#0000FF" scale={{ x: 3, y: 1, z: 0.2 }} />
-          </entity>
+      <scene position={{ x: 5, y: 0, z: 5 }}>
+        <entity rotation={doorRotation} transition={{ rotation: { duration: 1000, timing: 'ease-in' } }}>
+          <box id="door" scale={{ x: 1, y: 2, z: 0.05 }} position={{ x: 0.5, y: 1, z: 0 }} color="#00FF00" />
         </entity>
+        <box position={{ x: 2, y: 1, z: 0 }} scale={{ x: 2, y: 2, z: 0.05 }} color="#0000FF" />
+        <box position={{ x: -1, y: 1, z: 0 }} scale={{ x: 2, y: 2, z: 0.05 }} color="#0000FF" />
       </scene>
     )
   }
