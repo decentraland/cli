876ea383c1207ce56b1f94ace0bca050dc606087
Federico Miras <miras.federico@gmail.com>
2018-08-27 15:35:23 -0300

    fix: hot reloading wasn't working

--- src/commands/preview.ts
+++ src/commands/preview.ts
@@ -27,9 +27,12 @@ export function start(vorpal: any) {
     .description('Starts local development server.')
     .action(
       wrapCommand(async (args: IArguments) => {
+        const isCi = args.options.ci ? args.options.ci : false
+        const shouldWatchFiles = (args.options.watch !== undefined ? args.options.watch : true) && !isCi
+
         const dcl = new Decentraland({
           previewPort: args.options.port,
-          watch: args.options.watch || args.options.ci
+          watch: shouldWatchFiles
         })
 
         Analytics.preview()
@@ -66,7 +69,7 @@ export function start(vorpal: any) {
         dcl.on('preview:ready', port => {
           const ifaces = os.networkInterfaces()
 
-          const openBrowser = (args.options.browser ? args.options.browser : true) && (args.options.ci ? !args.options.ci : true)
+          const openBrowser = (args.options.browser !== undefined ? args.options.browser : true) && !isCi
           let url = null
 
           vorpal.log('') // line break
