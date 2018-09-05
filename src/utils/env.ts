a3a15e3e476d690c31b19fec8f26386da9d3b46c
Federico Miras <miras.federico@gmail.com>
2018-09-05 14:29:44 -0300

    feat: support now and CIs by default

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,4 +1,10 @@
+import { getOrElse } from '.'
+
 /**
  * Check if CLI is used in development mode.
  */
 export const isDev: boolean = process.env.DCL_ENV === 'dev'
+
+export function isEnvCi(): boolean {
+  return getOrElse(process.env.NOW, false) || getOrElse(process.env.CI, false)
+}
