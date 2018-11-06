c6d3aaa4c1484de220aa1f459645bf9cb0533c0b
menduz <agustin.mz.92@gmail.com>
2018-11-06 12:25:43 -0300

    chore: update example

--- src/samples/ecs/src/game.ts
+++ src/samples/ecs/src/game.ts
@@ -1,27 +1,51 @@
-export class RotatorSystem extends ComponentSystem {
-  constructor() {
-    super(Rotation)
-  }
+/// --- Set up a system ---
+
+class RotatorSystem extends System {
+  // this group will contain every entity that has a Transform component
+  group = engine.getComponentGroup(Transform)
+
   update(dt: number) {
-    for (let i in this.entities) {
-      const rotation = this.entities[i].get(Rotation)
-      rotation.y += dt * 10
+    // iterate over the entities of the group
+    for (let entity of this.group.entities) {
+      // get the Transform component of the entity
+      const transform = entity.get(Transform)
+
+      // mutate the rotation
+      transform.rotation.y += dt * 10
     }
   }
 }
 
-function spawn(x: number, y: number, z: number) {
+// Add a new instance of the system to the engine
+engine.addSystem(new RotatorSystem())
+
+/// --- Spawner function ---
+
+function spawnCube(x: number, y: number, z: number) {
+  // create the entity
   const cube = new Entity()
 
-  cube.set(new Position(x, y, z))
-  cube.set(new BoxShape())
-  cube.set(new Rotation(0, 0, 0))
+  // set a transform to the entity
+  cube.set(new Transform({ position: new Vector3(x, y, z) }))
 
+  // set a shape to the entity
+  cube.set(new BoxShape())
+
+  // add the entity to the engine
   engine.addEntity(cube)
 
   return cube
 }
 
-spawn(5, 1, 5)
+/// --- Spawn a cube ---
 
-engine.addSystem(new RotatorSystem())
+const cube = spawnCube(5, 1, 5)
+
+cube.set(
+  new OnClick(() => {
+    cube.get(Transform).scale.z *= 1.1
+    cube.get(Transform).scale.x *= 0.9
+
+    spawnCube(Math.random() * 8 + 1, Math.random() * 8, Math.random() * 8 + 1)
+  })
+)
