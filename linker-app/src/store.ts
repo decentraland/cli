3fde5557cccc8521fb252777808bfeb4e94d0e33
Federico Miras <miras.federico@gmail.com>
2018-12-14 15:25:59 -0300

    chore: update decentraland-eth (#299)

--- linker-app/src/store.ts
+++ linker-app/src/store.ts
@@ -10,7 +10,7 @@ import { isDevelopment } from './modules/config'
 
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const { storageMiddleware, loadStorageMiddleware } = createStorageMiddleware('decentraland-cli')
+const { storageMiddleware, loadStorageMiddleware } = createStorageMiddleware({ storageKey: 'decentraland-cli' })
 const sagasMiddleware = createSagasMiddleware()
 const transactionMiddleware = createTransactionMiddleware()
 const loggerMiddleware = createLogger({
