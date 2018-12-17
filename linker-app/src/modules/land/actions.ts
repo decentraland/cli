a9fd0b403abcb63f00f0c97484bd19321b6030da
Federico Miras <miras.federico@gmail.com>
2018-12-17 14:35:05 -0300

    feat: show error message when not update authorized on linker (#298)
    # What? <!-- what is this PR? -->
    Validates if the provided wallet address is authorized to update the base parcel of the scene.
    
    # Why? <!-- Explain the reason -->
    Because users could have different wallets/providers than the one that the CLI use and could cause a bad upload attempt.

--- linker-app/src/modules/land/actions.ts
+++ linker-app/src/modules/land/actions.ts
@@ -1,12 +1,12 @@
 import { action } from 'typesafe-actions'
-import { LANDMeta, Coords } from './types'
+import { LAND, Coords } from './types'
 
 export const FETCH_LAND_REQUEST = '[Request] Fetch LAND'
 export const FETCH_LAND_SUCCESS = '[Success] Fetch LAND'
 export const FETCH_LAND_FAILURE = '[Failure] Fetch LAND'
 
 export const fetchLandRequest = (coords: Coords) => action(FETCH_LAND_REQUEST, coords)
-export const fetchLandSuccess = (land: LANDMeta) => action(FETCH_LAND_SUCCESS, { land })
+export const fetchLandSuccess = (land: LAND) => action(FETCH_LAND_SUCCESS, { land })
 export const fetchLandFailure = (error: string) => action(FETCH_LAND_FAILURE, { error })
 
 export type FetchLandRequestAction = ReturnType<typeof fetchLandRequest>
