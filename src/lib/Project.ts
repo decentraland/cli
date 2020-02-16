ac5d485d01b9415fea3b03f21fcc1110138bdca8
Esteban Ordano <esteban@decentraland.org>
2020-02-16 08:51:03 -0300

    Revert "feat: warn that the "base" attribute is deprecated (#495)"
    This reverts commit d58633e954458cc8e4010be89157dbe6bf5d1640.

--- src/lib/Project.ts
+++ src/lib/Project.ts
@@ -13,7 +13,6 @@ import {
   getPackageFilePath
 } from '../utils/project'
 import { fail, ErrorType } from '../utils/errors'
-import { warning } from '../utils/logging'
 import { inBounds, getBounds, getObject, areConnected, Coords } from '../utils/coordinateHelpers'
 import { SceneMetadata } from '../sceneJson/types'
 
@@ -387,8 +386,8 @@ export class Project {
     const { base, parcels } = sceneFile.scene
     const parcelSet = new Set(parcels)
 
-    if (base) {
-      console.log(warning('The scene property "base" is deprecated!'))
+    if (!base) {
+      fail(ErrorType.PROJECT_ERROR, 'Missing scene base attribute at scene.json')
     }
 
     if (!parcels) {
