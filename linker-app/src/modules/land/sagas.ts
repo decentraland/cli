72608716c1aa54640b978828cb5bf79274e4ca21
Nicolas Chamo <nicolas@chamo.com.ar>
2021-01-27 09:22:32 -0300

    Add decodeLandData function

--- linker-app/src/modules/land/sagas.ts
+++ linker-app/src/modules/land/sagas.ts
@@ -1,6 +1,6 @@
 import { call, put, takeEvery } from 'redux-saga/effects'
 import { CONNECT_WALLET_SUCCESS } from 'decentraland-dapps/dist/modules/wallet/actions'
-
+import * as CSV from 'comma-separated-values'
 import { baseParcel } from '../../config'
 import { getLandContract } from '../../contracts'
 import {
@@ -22,7 +22,7 @@ function* handleFetchLandRequest(action: FetchLandRequestAction) {
   try {
     const { x, y } = action.payload
     const data = yield call(() => LANDRegistry['landData'](x, y))
-    const land = data ? yield call(() => LANDRegistry.decodeLandData(data)) : getEmptyLandData()
+    const land = data ? yield call(() => decodeLandData(data)) : getEmptyLandData()
     yield put(fetchLandSuccess(land))
   } catch (error) {
     yield put(fetchLandFailure(error.message))
@@ -31,4 +31,28 @@ function* handleFetchLandRequest(action: FetchLandRequestAction) {
 
 function* handleConnectWalletSuccess() {
   yield put(fetchLandRequest(baseParcel))
+}
+
+function decodeLandData(data = '') {
+  const version = data.charAt(0)
+  switch (version) {
+    case '0': {
+      const [version, name, description, ipns] = CSV.parse(data, {
+        cellDelimiter: ','
+      })[0]
+
+      return {
+        version,
+        // when a value is blank, csv.parse returns 0, so we fallback to empty string
+        // to support stuff like `0,,,ipns:link`
+        name: name || '',
+        description: description || '',
+        ipns: ipns || ''
+      }
+    }
+    default:
+      throw new Error(
+        `Unknown version when trying to decode land data: "${data}" (see https://github.com/decentraland/decentraland-eth/blob/master/docs/land-data.md)`
+      )
+  }
 }
