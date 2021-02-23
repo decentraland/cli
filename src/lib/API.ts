c9445056fe13a0b3731a2ebcd800b4462e63c079
Nicolas Chamo <nicolas@chamo.com.ar>
2021-02-23 15:37:14 -0300

    fix: use subgraph since API v1 was removed (#534)

--- src/lib/API.ts
+++ src/lib/API.ts
@@ -1,135 +1,177 @@
 import { LANDData } from './Ethereum'
 import { IEthereumDataProvider } from './IEthereumDataProvider'
 import { Coords } from '../utils/coordinateHelpers'
-import { fail, ErrorType } from '../utils/errors'
-import { filterAndFillEmpty } from '../utils/land'
 import { getConfig } from '../config'
+import { Fetcher } from 'dcl-catalyst-commons'
 
 const { dclApiUrl } = getConfig()
 
-const cache = new Map<string, any>()
-
-async function request(url) {
-  const cached = cache.get(url)
-  if (cached) {
-    return cached
-  }
-
-  try {
-    const res = await fetch(url)
-    const json = await res.json()
-    cache.set(url, json)
-    return json
-  } catch (e) {
-    fail(ErrorType.API_ERROR, `Unable to fetch from Decentraland remote API: ${e.message}`)
-  }
-}
-
 export class API implements IEthereumDataProvider {
-  async getEstateIdOfLand({ x, y }: Coords): Promise<number> {
-    const json = await request(`${dclApiUrl}/parcels/${x}/${y}`)
-    if (!json.data) {
-      return
-    }
+  private readonly fetcher: Fetcher = new Fetcher()
 
-    return json.data.estate_id
+  async getEstateIdOfLand({ x, y }: Coords): Promise<number> {
+    const query = `query GetEstateIdOfLand($x: Int!, $y: Int!) {
+      parcels(where: { x: $x, y: $y }) {
+        estate {
+          id
+        }
+      }
+    }`
+    type ResultType = { parcels: { estate: { id: string } | null }[] }
+    const response = await this.fetcher.queryGraph<ResultType>(dclApiUrl, query, { x, y })
+    const estate = response.parcels[0].estate
+    return estate ? parseInt(estate.id, 10) : undefined
   }
 
   async getEstateData(estateId: number): Promise<LANDData> {
-    const json = await request(`${dclApiUrl}/estates/${estateId}`)
-    if (!json.data) {
-      return
-    }
-
-    return filterAndFillEmpty(json.data.data)
+    const query = `query GetEstateData($estateId: String!) {
+      estates(where:{ id: $estateId }) {
+        data {
+          name
+          description
+        }
+      }
+    }`
+    type ResultType = { estates: { data: { name: string; description: string } | null }[] }
+    const response = await this.fetcher.queryGraph<ResultType>(dclApiUrl, query, {
+      estateId: `${estateId}`
+    })
+    return response.estates[0].data ? response.estates[0].data : { name: '', description: '' }
   }
   async getEstateOwner(estateId: number): Promise<string> {
-    const json = await request(`${dclApiUrl}/estates/${estateId}`)
-    if (!json.data) {
-      return
-    }
-
-    return json.data.owner
+    const query = `query GetEstateOwner($estateId: String!) {
+      estates(where:{ id: $estateId }) {
+        owner {
+          address
+        }
+      }
+    }`
+    type ResultType = { estates: { owner: { address: string } }[] }
+    const response = await this.fetcher.queryGraph<ResultType>(dclApiUrl, query, {
+      estateId: `${estateId}`
+    })
+    return response.estates[0].owner.address
   }
 
   async getEstateOperator(estateId: number): Promise<string> {
-    const json = await request(`${dclApiUrl}/estates/${estateId}`)
-    if (!json.data) {
-      return
-    }
-
-    return json.data.operator
+    const query = `query GetEstateOperator($estateId: String!) {
+      estates(where:{ id: $estateId }) {
+        operator
+      }
+    }`
+    type ResultType = { estates: { operator: string | null }[] }
+    const response = await this.fetcher.queryGraph<ResultType>(dclApiUrl, query, {
+      estateId: `${estateId}`
+    })
+    return response.estates[0].operator
   }
 
   async getEstateUpdateOperator(estateId: number): Promise<string> {
-    const json = await request(`${dclApiUrl}/estates/${estateId}`)
-    if (!json.data) {
-      return
-    }
-
-    return json.data.update_operator
+    const query = `query GetEstateUpdateOperator($estateId: String!) {
+      estates(where:{ id: $estateId }) {
+        updateOperator
+      }
+    }`
+    type ResultType = { estates: { updateOperator: string | null }[] }
+    const response = await this.fetcher.queryGraph<ResultType>(dclApiUrl, query, {
+      estateId: `${estateId}`
+    })
+    return response.estates[0].updateOperator
   }
 
   async getLandOfEstate(estateId: number): Promise<Coords[]> {
-    const json = await request(`${dclApiUrl}/estates/${estateId}`)
-    if (!json.data) {
-      return
-    }
-
-    return json.data.data.parcels
+    const query = `query GetLandOfEstate($estateId: String!) {
+      estates(where:{ id: $estateId }) {
+        parcels {
+          x
+          y
+        }
+      }
+    }`
+    type ResultType = { estates: { parcels: { x: string; y: string }[] }[] }
+    const response = await this.fetcher.queryGraph<ResultType>(dclApiUrl, query, {
+      estateId: `${estateId}`
+    })
+    return response.estates[0].parcels.map(parcel => ({
+      x: parseInt(parcel.x, 10),
+      y: parseInt(parcel.y, 10)
+    }))
   }
 
   async getLandData({ x, y }: Coords): Promise<LANDData> {
-    const json = await request(`${dclApiUrl}/parcels/${x}/${y}`)
-    if (!json.data) {
-      return
-    }
-
-    return filterAndFillEmpty(json.data.data)
+    const query = `query GetLandOwner($x: Int!, $y: Int!) {
+      parcels(where: { x: $x, y: $y }) {
+        data {
+          name
+          description
+          version
+        }
+      }
+    }`
+    type ResultType = { parcels: { data: { name: string; description: string } | null }[] }
+    const response = await this.fetcher.queryGraph<ResultType>(dclApiUrl, query, { x, y })
+    return response.parcels[0].data ? response.parcels[0].data : { name: '', description: '' }
   }
 
   async getLandOwner({ x, y }: Coords): Promise<string> {
-    const json = await request(`${dclApiUrl}/parcels/${x}/${y}`)
-    if (!json.data) {
-      return
-    }
-
-    return json.data.owner
+    const query = `query GetLandOwner($x: Int!, $y: Int!) {
+      parcels(where: { x: $x, y: $y }) {
+        owner {
+          address
+        }
+      }
+    }`
+    type ResultType = { parcels: { owner: { address: string } }[] }
+    const response = await this.fetcher.queryGraph<ResultType>(dclApiUrl, query, { x, y })
+    return response.parcels[0].owner.address
   }
 
   async getLandOperator({ x, y }: Coords): Promise<string> {
-    const json = await request(`${dclApiUrl}/parcels/${x}/${y}`)
-    if (!json.data) {
-      return
-    }
-
-    return json.data.operator
+    const query = `query GetLandOperator($x: Int!, $y: Int!) {
+      parcels(where: { x: $x, y: $y }) {
+        operator
+      }
+    }`
+    type ResultType = { parcels: { operator: string }[] }
+    const response = await this.fetcher.queryGraph<ResultType>(dclApiUrl, query, { x, y })
+    return response.parcels[0].operator
   }
 
   async getLandUpdateOperator({ x, y }: Coords): Promise<string> {
-    const json = await request(`${dclApiUrl}/parcels/${x}/${y}`)
-    if (!json.data) {
-      return
-    }
-
-    return json.data.update_operator
+    const query = `query GetLandUpdateOperator($x: Int!, $y: Int!) {
+      parcels(where: { x: $x, y: $y }) {
+        updateOperator
+      }
+    }`
+    type ResultType = { parcels: { updateOperator: string }[] }
+    const response = await this.fetcher.queryGraph<ResultType>(dclApiUrl, query, { x, y })
+    return response.parcels[0].updateOperator
   }
 
   async getLandOf(owner: string): Promise<Coords[]> {
-    const json = await request(`${dclApiUrl}/addresses/${owner}/parcels`)
-    if (!json.data) {
-      return
-    }
-
-    return json.data.map(({ x, y }) => ({ x, y }))
+    const query = `query GetLandOf($owner: String!) {
+      parcels(where: { owner: $owner }) {
+        x
+        y
+      }
+    }`
+    type ResultType = { parcels: { x: string; y: string }[] }
+    const response = await this.fetcher.queryGraph<ResultType>(dclApiUrl, query, {
+      owner: owner.toLowerCase()
+    })
+    return response.parcels.map(({ x, y }) => ({ x: parseInt(x, 10), y: parseInt(y, 10) }))
   }
 
   async getEstatesOf(owner: string): Promise<number[]> {
-    const json = await request(`${dclApiUrl}/addresses/${owner}/estates`)
-    if (!json.data) {
-      return
-    }
-
-    return json.data.map(({ id }) => id)
+    const query = `query GetEstatesOf($owner: String!) {
+      estates(where: { owner: $owner }) {
+        id
+      }
+    }`
+    type ResultType = { estates: { id: string }[] }
+    const response = await this.fetcher.queryGraph<ResultType>(dclApiUrl, query, {
+      owner: owner.toLowerCase()
+    })
+    return response.estates.map(({ id }) => parseInt(id, 10))
   }
 }
--- src/lib/content/ContentService.ts
+++ src/lib/content/ContentService.ts
@@ -1,18 +1,18 @@
 import { EventEmitter } from 'events'
 
-import { ContentClient, ParcelInformation } from './ContentClient'
 import { Coords } from '../../utils/coordinateHelpers'
 import { fail, ErrorType } from '../../utils/errors'
 import { SceneMetadata } from '../../sceneJson/types'
-
-const SCENE_FILE = 'scene.json'
+import { FileInfo } from '../Decentraland'
+import { CatalystClient } from 'dcl-catalyst-client'
+import { EntityType, Entity } from 'dcl-catalyst-commons'
 
 export class ContentService extends EventEmitter {
-  client: ContentClient
+  private readonly client: CatalystClient
 
-  constructor(client: ContentClient) {
+  constructor(catalystServerUrl: string) {
     super()
-    this.client = client
+    this.client = new CatalystClient(catalystServerUrl, 'CLI')
   }
 
   /**
@@ -20,15 +20,12 @@ export class ContentService extends EventEmitter {
    * @param x
    * @param y
    */
-  async getParcelStatus(coordinates: Coords): Promise<ParcelInformation> {
-    const response = await this.client.getParcelsInformation(coordinates, coordinates)
-    if (response.ok) {
-      return response.data.length > 0 ? response.data[0] : null
-    }
-    fail(
-      ErrorType.CONTENT_SERVER_ERROR,
-      `Error retrieving parcel ${coordinates.x},${coordinates.y} information: ${response.errorMessage}`
-    )
+  async getParcelStatus(coordinates: Coords): Promise<{ cid: string; files: FileInfo[] }> {
+    const entity = await this.fetchEntity(coordinates)
+    const content = entity.content
+      ? entity.content.map(entry => ({ name: entry.file, cid: entry.hash }))
+      : []
+    return { cid: entity.id, files: content }
   }
 
   /**
@@ -37,17 +34,24 @@ export class ContentService extends EventEmitter {
    * @param y
    */
   async getSceneData(coordinates: Coords): Promise<SceneMetadata> {
-    const information: ParcelInformation = await this.getParcelStatus(coordinates)
+    const entity = await this.fetchEntity(coordinates)
+    return entity.metadata
+  }
 
-    if (!information) {
-      return null
+  private async fetchEntity(coordinates: Coords): Promise<Entity> {
+    const pointer = `${coordinates.x},${coordinates.y}`
+    try {
+      const entities = await this.client.fetchEntitiesByPointers(EntityType.SCENE, [pointer])
+      const entity: Entity | undefined = entities[0]
+      if (!entity) {
+        fail(
+          ErrorType.CONTENT_SERVER_ERROR,
+          `Error retrieving parcel ${coordinates.x},${coordinates.y} information`
+        )
+      }
+      return entity
+    } catch (error) {
+      fail(ErrorType.CONTENT_SERVER_ERROR, error.message)
     }
-
-    const sceneFileCID = information.contents[SCENE_FILE]
-    if (!sceneFileCID) {
-      return null
-    }
-
-    return this.client.getContent(sceneFileCID)
   }
 }
