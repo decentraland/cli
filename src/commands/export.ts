c5377ea97a9fb22521ddbccfc017d1a0141cde9f
Leandro Mendoza <leandro@decentraland.org>
2021-08-06 09:50:08 -0300

    parameters as object, and default export in other function

--- src/commands/export.ts
+++ src/commands/export.ts
@@ -1,3 +1,4 @@
+import { MappingsFile } from './../lib/content/types'
 import * as path from 'path'
 import * as fs from 'fs-extra'
 import * as arg from 'arg'
@@ -75,61 +76,77 @@ export async function main(): Promise<number> {
 
   const dclEcsPath = path.resolve(workDir, 'node_modules', 'decentraland-ecs')
   const exportSetupPath = path.resolve(dclEcsPath, 'src/setupExport.js')
-  let externalExport = null
+  let exportDependencies: any = defaultExport
 
   if (fs.existsSync(exportSetupPath)) {
     try {
-      externalExport = __non_webpack_require__(exportSetupPath)
+      const externalExport = __non_webpack_require__(exportSetupPath)
+      exportDependencies = externalExport
     } catch (err) {
       console.log(`${exportSetupPath} found but it couldn't be loaded properly`)
     }
   }
 
-  if (externalExport) {
-    externalExport(exportDir, sceneJson.display.title, mappings)
-  } else {
-    const artifactPath = path.resolve(workDir, 'node_modules', 'decentraland-ecs', 'artifacts')
-
-    // Change HTML title name
-    const content = await fs.readFile(path.resolve(artifactPath, 'export.html'), 'utf-8')
-    const finalContent = content.replace('{{ scene.display.title }}', sceneJson.display.title)
-
-    try {
-      // decentraland-ecs <= 6.6.4
-      await fs.copy(path.resolve(artifactPath, 'unity'), path.resolve(exportDir, 'unity'))
-    } catch {
-      // decentraland-ecs > 6.6.4
-      await fs.copy(
-        path.resolve(artifactPath, 'unity-renderer'),
-        path.resolve(exportDir, 'unity-renderer')
-      )
-    }
-
-    await Promise.all([
-      fs.writeFile(path.resolve(exportDir, 'index.html'), finalContent, 'utf-8'),
-      fs.writeFile(path.resolve(exportDir, 'mappings'), JSON.stringify(mappings), 'utf-8'),
-
-      fs.copy(path.resolve(artifactPath, 'preview.js'), path.resolve(exportDir, 'preview.js')),
-
-      fs.copy(
-        path.resolve(artifactPath, 'default-profile'),
-        path.resolve(exportDir, 'default-profile')
-      ),
-      fs.copy(
-        path.resolve(artifactPath, 'images/decentraland-connect'),
-        path.resolve(exportDir, 'images/decentraland-connect')
-      ),
-      fs.copy(
-        path.resolve(artifactPath, 'images/progress-logo.png'),
-        path.resolve(exportDir, 'images/progress-logo.png')
-      ),
-      fs.copy(
-        path.resolve(artifactPath, 'images/teleport.gif'),
-        path.resolve(exportDir, 'images/teleport.gif')
-      )
-    ])
-  }
+  await exportDependencies({
+    workDir,
+    exportDir,
+    mappings,
+    sceneJson
+  })
 
   spinner.succeed('Export successful.')
   return 0
+}
+
+async function defaultExport({
+  workDir,
+  exportDir,
+  mappings,
+  sceneJson
+}: {
+  workDir: string
+  exportDir: string
+  mappings: MappingsFile
+  sceneJson: any
+}): Promise<void> {
+  const artifactPath = path.resolve(workDir, 'node_modules', 'decentraland-ecs', 'artifacts')
+
+  // Change HTML title name
+  const content = await fs.readFile(path.resolve(artifactPath, 'export.html'), 'utf-8')
+  const finalContent = content.replace('{{ scene.display.title }}', sceneJson.display.title)
+
+  try {
+    // decentraland-ecs <= 6.6.4
+    await fs.copy(path.resolve(artifactPath, 'unity'), path.resolve(exportDir, 'unity'))
+  } catch {
+    // decentraland-ecs > 6.6.4
+    await fs.copy(
+      path.resolve(artifactPath, 'unity-renderer'),
+      path.resolve(exportDir, 'unity-renderer')
+    )
+  }
+
+  await Promise.all([
+    fs.writeFile(path.resolve(exportDir, 'index.html'), finalContent, 'utf-8'),
+    fs.writeFile(path.resolve(exportDir, 'mappings'), JSON.stringify(mappings), 'utf-8'),
+
+    fs.copy(path.resolve(artifactPath, 'preview.js'), path.resolve(exportDir, 'preview.js')),
+
+    fs.copy(
+      path.resolve(artifactPath, 'default-profile'),
+      path.resolve(exportDir, 'default-profile')
+    ),
+    fs.copy(
+      path.resolve(artifactPath, 'images/decentraland-connect'),
+      path.resolve(exportDir, 'images/decentraland-connect')
+    ),
+    fs.copy(
+      path.resolve(artifactPath, 'images/progress-logo.png'),
+      path.resolve(exportDir, 'images/progress-logo.png')
+    ),
+    fs.copy(
+      path.resolve(artifactPath, 'images/teleport.gif'),
+      path.resolve(exportDir, 'images/teleport.gif')
+    )
+  ])
 }
