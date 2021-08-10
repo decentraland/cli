3f817ec1638e5595c79dc4735b1d4f2c38f20a1b
Leandro Mendoza <leandro@decentraland.org>
2021-08-10 17:21:42 -0300

    feat: make dcl install (#573)
    * feat: look for decentralandLibrary packages and add to bundleDependencies
    
    * feat: add bundleDependencies' check on every dcl start
    
    * feat: add dcl install package command which runs npm install package

---
+++ src/commands/install.ts
@@ -0,0 +1,66 @@
+import * as arg from 'arg'
+import chalk from 'chalk'
+import updateBundleDependenciesField from '../project/updateBundleDependenciesField'
+import { spawn } from 'child_process'
+import { npm } from './../utils/moduleHelpers'
+import * as spinner from '../utils/spinner'
+
+export const help = () => `
+  Usage: ${chalk.bold('dcl install [package]')}
+
+    ${chalk.dim('Options:')}
+
+      -h, --help               Displays complete help
+
+    ${chalk.dim('Examples:')}
+
+    - Install a new package
+
+      ${chalk.green('$ dcl install package-example')}
+
+    - Check the Decentraland libraries used are in bundleDependencies
+
+      ${chalk.green('$ dcl install')}
+`
+
+const spawnNpmInstall = (args: any): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    spinner.create(`npm ${args.join(' ')}`)
+
+    const child = spawn(npm, args, {
+      shell: true,
+      cwd: process.cwd(),
+      env: { ...process.env, NODE_ENV: '' }
+    })
+
+    child.stdout.pipe(process.stdout)
+    child.stderr.pipe(process.stderr)
+
+    child.on('close', code => {
+      if (code !== 0) {
+        spinner.fail()
+        reject(
+          new Error(
+            `${chalk.bold(
+              `npm ${args.join(' ')}`
+            )} exited with code ${code}. Please try running the command manually`
+          )
+        )
+      } else {
+        spinner.succeed()
+        resolve()
+      }
+    })
+  })
+}
+
+export async function main() {
+  const args = arg({
+    '--help': Boolean,
+    '-h': '--help'
+  })
+
+  await spawnNpmInstall(args._)
+
+  await updateBundleDependenciesField()
+}
--- src/main.ts
+++ src/main.ts
@@ -38,6 +38,8 @@ const help = `
       init                Create a new Decentraland Scene project
       build               Build scene
       start               Start a local development server for a Decentraland Scene
+      install             Sync decentraland libraries in bundleDependencies
+      install package     Install a package
       deploy              Upload scene to a particular Decentraland's Content server
       deploy-deprecated   Upload scene to Decentraland's legacy content server (deprecated).
       export              Export scene to static website format (HTML, JS and CSS)
