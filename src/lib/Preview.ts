6b536748bdea556bef2ba08d48efd74e9e3b862d
Leandro Mendoza <leandro@decentraland.org>
2021-11-18 16:51:19 -0300

    add payload to update message sceneType and sceneId

--- src/lib/Preview.ts
+++ src/lib/Preview.ts
@@ -58,32 +58,52 @@ export class Preview extends EventEmitter {
         resolvedPort = 2044
       }
     }
+
+    // PoC - TODO: need to be typed
+    let sceneId: string = ''
+    let sceneType: string = 'parcel'
+    const assetJsonPath = path.resolve(this.dcl.getWorkingDir(), 'asset.json')
+    if (fs.existsSync(assetJsonPath)) {
+      try {
+        const assetJson: any = require(assetJsonPath)
+        if (assetJson?.id && assetJson?.assetType === 'portable-experience'){
+          sceneId = assetJson.id
+          sceneType = 'portable-experience'
+        }
+      } catch (err) {}
+    }
+
     const ig = ignore().add(this.ignoredPaths)
     if (this.watch) {
-      chokidar
-        .watch(this.dcl.getWorkingDir())
-        .on('all', (_, pathWatch) => {
-          if (ig.ignores(pathWatch)) {
-            return
+      chokidar.watch(this.dcl.getWorkingDir()).on('all', (_, pathWatch) => {
+        if (ig.ignores(pathWatch)) {
+          return
+        }
+
+        this.wss.clients.forEach(ws => {
+          if (
+            ws.readyState === WebSocket.OPEN &&
+            (!ws.protocol || ws.protocol === 'scene-updates')
+          ) {
+            ws.send('update')
+
+            ws.send(
+              JSON.stringify({
+                type: 'update',
+                cwd: this.dcl.getWorkingDir(),
+                path: relativiseUrl(pathWatch),
+                payload: {
+                  type: 'watch',
+                  body:{ 
+                    sceneId,
+                    sceneType
+                  }
+                }
+              })
+            )
           }
-
-          this.wss.clients.forEach(ws => {
-            if (
-              ws.readyState === WebSocket.OPEN &&
-              (!ws.protocol || ws.protocol === 'scene-updates')
-            ) {
-              ws.send('update')
-
-              ws.send(
-                JSON.stringify({
-                  type: 'update',
-                  cwd: this.dcl.getWorkingDir(),
-                  path: relativiseUrl(pathWatch)
-                })
-              )
-            }
-          })
         })
+      })
     }
 
     this.app.use(cors())
