9b6421a8cf145cb84a9ec9bd029476eff7f9d6e1
Leandro Mendoza <leandro@decentraland.org>
2022-03-18 16:40:16 -0300

    feat: extend `scene` option in `dcl init` to choose between local example and remote examples.

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,4 +1,4 @@
-import inquirer, { Questions } from 'inquirer'
+import inquirer, { ChoiceType, Questions } from 'inquirer'
 import chalk from 'chalk'
 import arg from 'arg'
 import { sdk } from '@dcl/schemas'
@@ -11,6 +11,7 @@ import installDependencies from '../project/installDependencies'
 
 import { isEmptyDirectory } from '../utils/filesystem'
 import { ProjectType } from '@dcl/schemas/dist/sdk'
+import { downloadRepo } from '../utils/shellCommands'
 
 export const help = () => `
   Usage: ${chalk.bold('dcl init [options]')}
@@ -39,45 +40,212 @@ function getProjectTypes() {
     .join(', ')
 }
 
-async function getprojectType(type?: string): Promise<sdk.ProjectType> {
-  if (!type) {
-    const choices = [
-      { value: sdk.ProjectType.SCENE, name: 'Scene' },
-      { value: sdk.ProjectType.SMART_ITEM, name: 'Smart Item' },
-      {
-        value: sdk.ProjectType.PORTABLE_EXPERIENCE,
-        name: 'Smart Wearable (Beta)'
-      },
-      {
-        value: sdk.ProjectType.LIBRARY,
-        name: 'Library'
-      }
-    ]
+type InitOptionProjectType = {
+  type: 'sdk.ProjectType'
+  value: sdk.ProjectType
+}
 
-    const projectTypeList: Questions = [
-      {
-        type: 'list',
-        name: 'project',
-        message: 'Choose a project type',
-        choices
-      }
-    ]
-    const answers = await inquirer.prompt(projectTypeList)
-    const projectType: sdk.ProjectType = answers.project
+type InitOptionRepositoryURL = {
+  type: 'repository.URL'
+  value: string
+}
 
-    return projectType
+type InitOption = InitOptionProjectType | InitOptionRepositoryURL
+
+async function getSceneInitOption(): Promise<InitOption> {
+  const remoteRepositories = [
+    {
+      title: 'Grass noise example',
+      url: 'https://github.com/decentraland-scenes/grass-noise-example.git'
+    },
+    {
+      title: 'Basic interactions',
+      url: 'https://github.com/decentraland-scenes/Basic-Interactions'
+    },
+    {
+      title: 'Moving platforms',
+      url: 'https://github.com/decentraland-scenes/moving-platforms'
+    },
+    {
+      title: 'Video streaming',
+      url: 'https://github.com/decentraland-scenes/video-streaming'
+    },
+    {
+      title: 'Lazy loading',
+      url: 'https://github.com/decentraland-scenes/lazy-loading'
+    },
+    {
+      title: 'POAP booth',
+      url: 'https://github.com/decentraland-scenes/POAP-Booth'
+    }
+  ]
+  const remoteChoices = remoteRepositories.map((repo) => ({
+    name: repo.title,
+    value: repo.url
+  }))
+
+  const choices: ChoiceType[] = [
+    {
+      name: 'Local example',
+      value: sdk.ProjectType.SCENE
+    },
+    {
+      name: 'Paste a repository URL',
+      value: 'write-repository'
+    },
+    new inquirer.Separator(),
+    ...remoteChoices,
+    new inquirer.Separator()
+  ]
+
+  const projectTypeList: Questions = [
+    {
+      type: 'list',
+      name: 'scene',
+      message: 'Choose a scene',
+      choices
+    }
+  ]
+  const answers = await inquirer.prompt(projectTypeList)
+  if (answers.scene === sdk.ProjectType.SCENE) {
+    return {
+      type: 'sdk.ProjectType',
+      value: sdk.ProjectType.SCENE
+    }
+  } else if (answers.scene === 'write-repository') {
+    const answers = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'url',
+        message: 'Write the repository URL:'
+      }
+    ])
+    return {
+      type: 'repository.URL',
+      value: answers.url
+    }
+  } else {
+    const choice = remoteChoices.find(
+      (item) => item.value === answers.scene
+    )! as any
+
+    if (choice) {
+      return {
+        type: 'repository.URL',
+        value: answers.scene
+      }
+    }
   }
 
-  if (!sdk.ProjectType.validate(type)) {
-    fail(
-      ErrorType.INIT_ERROR,
-      `Invalid projectType: "${chalk.bold(
-        type
-      )}". Supported types are ${chalk.bold(getProjectTypes())}`
-    )
+  fail(
+    ErrorType.INIT_ERROR,
+    `Couldn't get a valid scene-level choice. Try to select a valid one.`
+  )
+  return {} as any
+}
+
+async function getInitOption(type?: string): Promise<InitOption> {
+  if (type) {
+    if (!sdk.ProjectType.validate(type)) {
+      fail(
+        ErrorType.INIT_ERROR,
+        `Invalid projectType: "${chalk.bold(
+          type
+        )}". Supported types are ${chalk.bold(getProjectTypes())}`
+      )
+    }
+
+    return {
+      type: 'sdk.ProjectType',
+      value: type as sdk.ProjectType
+    }
   }
 
-  return type as sdk.ProjectType
+  const firstChoices: ChoiceType[] = [
+    {
+      name: 'Scene',
+      value: 'scene-option'
+    },
+    {
+      name: 'Smart Item',
+      value: sdk.ProjectType.SMART_ITEM
+    },
+    {
+      name: 'Smart Wearable (Beta)',
+      value: sdk.ProjectType.PORTABLE_EXPERIENCE
+    },
+    {
+      name: 'Library',
+      value: sdk.ProjectType.LIBRARY
+    }
+  ]
+
+  const projectTypeList: Questions = [
+    {
+      type: 'list',
+      name: 'project',
+      message: 'Choose a project type',
+      choices: firstChoices
+    }
+  ]
+  const answers = await inquirer.prompt(projectTypeList)
+
+  if (sdk.ProjectType.validate(answers.project)) {
+    return {
+      type: 'sdk.ProjectType',
+      value: answers.project
+    }
+  }
+
+  if (answers.project === 'scene-option') {
+    return await getSceneInitOption()
+  }
+
+  fail(
+    ErrorType.INIT_ERROR,
+    `Couldn't get a valid first-level choice. Try to select a valid one.`
+  )
+  return {} as any
+}
+
+export async function initProjectType(
+  dcl: Decentraland,
+  projectType: sdk.ProjectType
+) {
+  const project = dcl.workspace.getSingleProject()!
+
+  if (projectType !== ProjectType.LIBRARY) {
+    await project.writeDclIgnore()
+    await project.writeSceneFile({})
+  }
+
+  await project.scaffoldProject(projectType)
+
+  try {
+    await installDependencies(dcl.getWorkingDir(), false)
+  } catch (error: any) {
+    fail(ErrorType.INIT_ERROR, error.message)
+  }
+
+  Analytics.sceneCreated({ projectType: projectType })
+
+  if (projectType !== ProjectType.LIBRARY) {
+    console.log(chalk.green(`\nSuccess! Run 'dcl start' to see your scene\n`))
+  }
+}
+
+async function initRepository(dcl: Decentraland, url: string) {
+  const project = dcl.workspace.getSingleProject()!
+
+  await downloadRepo(project.getProjectWorkingDir(), url, '.')
+
+  try {
+    await installDependencies(dcl.getWorkingDir(), false)
+  } catch (error: any) {
+    fail(ErrorType.INIT_ERROR, error.message)
+  }
+
+  Analytics.sceneCreated({ projectType: 'scene-template', url })
 }
 
 export async function main() {
@@ -106,33 +274,19 @@ export async function main() {
   }
 
   if (!project) {
-    fail(
-      ErrorType.INIT_ERROR,
-      'Cannot try to init a project in workspace directory'
-    )
+    fail(ErrorType.INIT_ERROR, 'Cannot init a project in workspace directory')
     return
   }
 
   await project.validateNewProject()
 
-  const projectType = await getprojectType(args['--project'])
+  const choice = await getInitOption(args['--project'])
 
-  if (projectType !== ProjectType.LIBRARY) {
-    await project.writeDclIgnore()
-    await project.writeSceneFile({})
-  }
-
-  await project.scaffoldProject(projectType)
-
-  try {
-    await installDependencies(dcl.getWorkingDir(), false)
-  } catch (error: any) {
-    fail(ErrorType.INIT_ERROR, error.message)
-  }
-
-  Analytics.sceneCreated({ projectType: projectType })
-
-  if (projectType !== ProjectType.LIBRARY) {
-    console.log(chalk.green(`\nSuccess! Run 'dcl start' to see your scene\n`))
+  if (choice.type === 'sdk.ProjectType') {
+    await initProjectType(dcl, choice.value)
+  } else if (choice.type === 'repository.URL') {
+    await initRepository(dcl, choice.value)
+  } else {
+    fail(ErrorType.INIT_ERROR, 'Cannot get a choice')
   }
 }
---
+++ src/utils/shellCommands.ts
@@ -0,0 +1,97 @@
+import { spawn } from 'child_process'
+
+const npm = /^win/.test(process.platform) ? 'npm.cmd' : 'npm'
+
+enum FileDescriptorStandardOption {
+  SILENT = 1,
+  PIPE = 2,
+  ONLY_IF_THROW = 3
+}
+
+export function runCommand({
+  workingDir,
+  command,
+  args,
+  fdStandards
+}: {
+  workingDir: string
+  command: string
+  args: string[]
+  fdStandards?: FileDescriptorStandardOption
+}): Promise<void> {
+  const standarOption = fdStandards || FileDescriptorStandardOption.SILENT
+  return new Promise((resolve, reject) => {
+    const child = spawn(command, args, {
+      shell: true,
+      cwd: workingDir,
+      env: { ...process.env, NODE_ENV: '' }
+    })
+
+    let stdOut = ''
+    let stdErr = ''
+
+    if (standarOption === FileDescriptorStandardOption.PIPE) {
+      child.stdout.pipe(process.stdout)
+      child.stderr.pipe(process.stderr)
+    } else if (standarOption === FileDescriptorStandardOption.ONLY_IF_THROW) {
+      child.stdout.on('data', (data) => {
+        stdOut += data.toString()
+      })
+
+      child.stderr.on('data', (data) => {
+        stdErr += data.toString()
+      })
+    }
+
+    child.on('close', (code) => {
+      const errorMessage = `Command '${command}' with args '${args.join(
+        ' '
+      )}' exited with code ${code}. \n
+          > Working directory: ${workingDir} `
+
+      if (code !== 0) {
+        if (standarOption === FileDescriptorStandardOption.ONLY_IF_THROW) {
+          reject(
+            new Error(`${errorMessage} \n
+            > Standard output: \n ${stdOut} \n
+            > Error output: \n ${stdErr} \n`)
+          )
+        } else {
+          reject(new Error(errorMessage))
+        }
+      }
+      resolve()
+    })
+  })
+}
+
+export function installDependencies(workingDir: string): Promise<void> {
+  return runCommand({
+    workingDir,
+    command: npm,
+    args: ['install'],
+    fdStandards: FileDescriptorStandardOption.SILENT
+  })
+}
+
+export function runDclBuild(workingDir: string): Promise<void> {
+  return runCommand({
+    workingDir,
+    command: 'dcl',
+    args: ['build'],
+    fdStandards: FileDescriptorStandardOption.ONLY_IF_THROW
+  })
+}
+
+export function downloadRepo(
+  workingDir: string,
+  url: string,
+  destinationPath: string
+): Promise<void> {
+  return runCommand({
+    workingDir,
+    command: 'git',
+    args: ['clone', '--depth', '1', url, destinationPath],
+    fdStandards: FileDescriptorStandardOption.PIPE
+  })
+}
