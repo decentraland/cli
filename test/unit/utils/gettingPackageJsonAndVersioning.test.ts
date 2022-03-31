da0a2a2b82dc731e817fba9dcb91875c38ba3c6f
Leandro Mendoza <leandro@decentraland.org>
2022-03-31 10:12:49 -0300

    fix test

---
+++ test/unit/utils/gettingPackageJsonAndVersioning.test.ts
@@ -0,0 +1,34 @@
+import test from 'ava'
+import { getCLIPackageJson } from '../../../src/utils/moduleHelpers'
+import {
+  getNodeVersion,
+  getNpmVersion
+} from '../../../src/utils/nodeAndNpmVersion'
+
+test('Unit - getCLIPackageJson() - should have userEngines', async (t) => {
+  const requiredVersion = await getCLIPackageJson<{
+    userEngines: {
+      minNodeVersion: string
+      minNpmVersion: string
+    }
+  }>()
+
+  t.deepEqual(requiredVersion.userEngines, {
+    minNodeVersion: '14.0.0',
+    minNpmVersion: '6.0.0'
+  })
+})
+
+test('Unit - npm and node versions - having installed node and npm should return valid values', async (t) => {
+  const nodeVersion = await getNodeVersion()
+  const npmVersion = await getNpmVersion()
+
+  t.true(typeof nodeVersion === 'string')
+  t.true(typeof npmVersion === 'string')
+
+  const majorNodeVersion = parseInt(nodeVersion.split('.')[0])
+  const majorNpmVersion = parseInt(npmVersion.split('.')[0])
+
+  t.true(majorNpmVersion >= 7)
+  t.true(majorNodeVersion >= 16)
+})
