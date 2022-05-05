af0a9831f8cebb53acf57d3d5d819e4ac1231611
menduz <github@menduz.com>
2022-05-05 17:27:15 -0300

    fix linter

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,33 +1,50 @@
-import { v4 as uuidv4 } from "uuid"
-import AnalyticsNode from "analytics-node"
-import { createDCLInfo, getConfig } from "../config"
-import { isOnline, getInstalledCLIVersion, getInstalledVersion } from "./moduleHelpers"
-import { debug } from "./logging"
-import chalk from "chalk"
-import { debuglog } from "util"
+import { v4 as uuidv4 } from 'uuid'
+import AnalyticsNode from 'analytics-node'
+import { createDCLInfo, getConfig } from '../config'
+import {
+  isOnline,
+  getInstalledCLIVersion,
+  getInstalledVersion
+} from './moduleHelpers'
+import { debug } from './logging'
+import chalk from 'chalk'
+import { debuglog } from 'util'
 
 // Setup segment.io
-const SINGLEUSER = "cli-user"
+const SINGLEUSER = 'cli-user'
 
 export let analytics: AnalyticsNode
 
-const ANONYMOUS_DATA_QUESTION = "Send Anonymous data"
+const ANONYMOUS_DATA_QUESTION = 'Send Anonymous data'
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace Analytics {
-  export const sceneCreated = (properties?: any) => trackAsync("Scene created", properties)
-  export const preview = (properties?: any) => trackAsync("Preview started", properties)
-  export const sceneDeploy = (properties?: any) => trackAsync("Scene deploy started", properties)
-  export const sceneDeploySuccess = (properties?: any) => trackAsync("Scene deploy success", properties)
-  export const sceneLink = (properties?: any) => trackAsync("Scene ethereum link started", properties)
-  export const sceneLinkSuccess = (properties?: any) => trackAsync("Scene ethereum link succeeded", properties)
-  export const deploy = (properties?: any) => trackAsync("Scene deploy requested", properties)
-  export const pinRequest = (properties?: any) => trackAsync("Pin requested", properties)
-  export const pinSuccess = (properties?: any) => trackAsync("Pin success", properties)
-  export const infoCmd = (properties?: any) => trackAsync("Info command", properties)
-  export const statusCmd = (properties?: any) => trackAsync("Status command", properties)
-  export const sendData = (shareData: boolean) => trackAsync(ANONYMOUS_DATA_QUESTION, { shareData })
-  export const tryToUseDeprecated = (properties?: any) => trackAsync("Try to use depacreated feature", properties)
+  export const sceneCreated = (properties?: any) =>
+    trackAsync('Scene created', properties)
+  export const preview = (properties?: any) =>
+    trackAsync('Preview started', properties)
+  export const sceneDeploy = (properties?: any) =>
+    trackAsync('Scene deploy started', properties)
+  export const sceneDeploySuccess = (properties?: any) =>
+    trackAsync('Scene deploy success', properties)
+  export const sceneLink = (properties?: any) =>
+    trackAsync('Scene ethereum link started', properties)
+  export const sceneLinkSuccess = (properties?: any) =>
+    trackAsync('Scene ethereum link succeeded', properties)
+  export const deploy = (properties?: any) =>
+    trackAsync('Scene deploy requested', properties)
+  export const pinRequest = (properties?: any) =>
+    trackAsync('Pin requested', properties)
+  export const pinSuccess = (properties?: any) =>
+    trackAsync('Pin success', properties)
+  export const infoCmd = (properties?: any) =>
+    trackAsync('Info command', properties)
+  export const statusCmd = (properties?: any) =>
+    trackAsync('Status command', properties)
+  export const sendData = (shareData: boolean) =>
+    trackAsync(ANONYMOUS_DATA_QUESTION, { shareData })
+  export const tryToUseDeprecated = (properties?: any) =>
+    trackAsync('Try to use depacreated feature', properties)
 
   export async function identify(devId: string) {
     try {
@@ -36,22 +53,29 @@ export namespace Analytics {
         traits: {
           os: process.platform,
           createdAt: new Date().getTime(),
-          isCI: process.env.CI === "true" || (process.argv.includes("--ci") && process.argv.includes("--c")),
-          devId,
-        },
+          isCI:
+            process.env.CI === 'true' ||
+            (process.argv.includes('--ci') && process.argv.includes('--c')),
+          devId
+        }
       })
     } catch (err: any) {
-      debuglog("" + err)
+      debuglog('' + err)
     }
   }
 
-  export async function reportError(workingDir: string, type: string, message: string, stackTrace: string) {
+  export async function reportError(
+    workingDir: string,
+    type: string,
+    message: string,
+    stackTrace: string
+  ) {
     return track(
-      "Error",
+      'Error',
       {
         errorType: type,
         message,
-        stackTrace,
+        stackTrace
       },
       workingDir
     )
@@ -65,14 +89,14 @@ export namespace Analytics {
       console.log(
         chalk.dim(
           `Decentraland CLI sends anonymous usage stats to improve their products, if you want to disable it change the configuration at ${chalk.bold(
-            "~/.dclinfo"
+            '~/.dclinfo'
           )}\n`
         )
       )
 
       const newUserId = uuidv4()
       await createDCLInfo({ userId: newUserId, trackStats: true })
-      debug(`${chalk.bold(".dclinfo")} file created`)
+      debug(`${chalk.bold('.dclinfo')} file created`)
       await identify(newUserId)
       sendData(true)
     }
@@ -84,7 +108,11 @@ export namespace Analytics {
  * @param eventName The name of the event to be tracked
  * @param properties Any object containing serializable data
  */
-async function track(eventName: string, properties: any = {}, workingDir?: string) {
+async function track(
+  eventName: string,
+  properties: any = {},
+  workingDir?: string
+) {
   const { userId, trackStats } = getConfig()
 
   if (!(await isOnline())) {
@@ -92,14 +120,20 @@ async function track(eventName: string, properties: any = {}, workingDir?: strin
   }
 
   return new Promise<void>(async (resolve) => {
-    const dclApiVersion = await getInstalledVersion(workingDir!, "decentraland-api")
+    const dclApiVersion = await getInstalledVersion(
+      workingDir!,
+      'decentraland-api'
+    )
     const newProperties = {
       ...properties,
       os: process.platform,
       nodeVersion: process.version,
       cliVersion: getInstalledCLIVersion(),
-      isCI: process.env.CI === "true" || (process.argv.includes("--ci") && process.argv.includes("--c")),
-      devId: userId,
+      isCI:
+        process.env.CI === 'true' ||
+        process.argv.includes('--ci') ||
+        process.argv.includes('--c'),
+      devId: userId
     }
 
     const shouldTrack = trackStats || eventName === ANONYMOUS_DATA_QUESTION
@@ -116,7 +150,7 @@ async function track(eventName: string, properties: any = {}, workingDir?: strin
     const event = {
       userId: SINGLEUSER,
       event: eventName,
-      properties: newProperties,
+      properties: newProperties
     }
 
     try {
@@ -124,7 +158,7 @@ async function track(eventName: string, properties: any = {}, workingDir?: strin
         resolve()
       })
     } catch (e) {
-      debuglog("" + e)
+      debuglog('' + e)
       resolve()
     }
   })
