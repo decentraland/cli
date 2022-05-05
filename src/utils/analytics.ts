e01cb51edbce3fb3bd021c1fb9dce7a59759e04a
menduz <github@menduz.com>
2022-05-05 17:58:37 -0300

    fix: do not fail if firewall prevents analytics.identify (#687)
    * fix: prevent failures if firewall is preventing analytics.identify
    
    * fix linter
    
    * fix duplicated condition

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -8,6 +8,7 @@ import {
 } from './moduleHelpers'
 import { debug } from './logging'
 import chalk from 'chalk'
+import { debuglog } from 'util'
 
 // Setup segment.io
 const SINGLEUSER = 'cli-user'
@@ -46,17 +47,22 @@ export namespace Analytics {
     trackAsync('Try to use depacreated feature', properties)
 
   export async function identify(devId: string) {
-    analytics.identify({
-      userId: SINGLEUSER,
-      traits: {
-        os: process.platform,
-        createdAt: new Date().getTime(),
-        isCI:
-          process.env.CI === 'true' ||
-          (process.argv.includes('--ci') && process.argv.includes('--c')),
-        devId
-      }
-    })
+    try {
+      analytics.identify({
+        userId: SINGLEUSER,
+        traits: {
+          os: process.platform,
+          createdAt: new Date().getTime(),
+          isCI:
+            process.env.CI === 'true' ||
+            process.argv.includes('--ci') ||
+            process.argv.includes('--c'),
+          devId
+        }
+      })
+    } catch (err: any) {
+      debuglog('' + err)
+    }
   }
 
   export async function reportError(
@@ -126,7 +132,8 @@ async function track(
       cliVersion: getInstalledCLIVersion(),
       isCI:
         process.env.CI === 'true' ||
-        (process.argv.includes('--ci') && process.argv.includes('--c')),
+        process.argv.includes('--ci') ||
+        process.argv.includes('--c'),
       devId: userId
     }
 
@@ -152,6 +159,7 @@ async function track(
         resolve()
       })
     } catch (e) {
+      debuglog('' + e)
       resolve()
     }
   })
