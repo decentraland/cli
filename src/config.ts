5e40b48af77f9ffa876c12af1e61a5215298ddfe
Gon Pombo <gonzalo@decentraland.org>
2022-08-16 12:04:25 -0300

    chore: change testnet to goerli (#772)
    * update testnet to goerli
    
    * fix tests
    
    * update linker-dapp to use goerli
    
    * fix snapshots

--- src/config.ts
+++ src/config.ts
@@ -92,23 +92,23 @@ function getDefaultConfig(network: string): Partial<DCLInfo> {
     userId: '',
     trackStats: false,
     provider: isDevelopment()
-      ? 'https://ropsten.infura.io/'
+      ? 'https://goerli.infura.io/'
       : 'https://mainnet.infura.io/',
     MANAToken: isMainnet
       ? '0x0f5d2fb29fb7d3cfee444a200298f468908cc942'
-      : '0x2a8fd99c19271f4f04b1b7b9c4f7cf264b626edb',
+      : '0xe7fDae84ACaba2A5Ba817B6E6D8A2d415DBFEdbe',
     LANDRegistry: isMainnet
       ? '0xf87e31492faf9a91b02ee0deaad50d51d56d5d4d'
-      : '0x7a73483784ab79257bb11b96fd62a2c3ae4fb75b',
+      : '0x25b6B4bac4aDB582a0ABd475439dA6730777Fbf7',
     EstateRegistry: isMainnet
       ? '0x959e104e1a4db6317fa58f8295f586e1a978c297'
-      : '0x124bf28a423b2ca80b3846c3aa0eb944fe7ebb95',
+      : '0xC9A46712E6913c24d15b46fF12221a79c4e251DC',
     catalystUrl: isMainnet
       ? 'https://peer.decentraland.org'
       : 'https://peer.decentraland.zone',
     dclApiUrl: isMainnet
       ? 'https://api.thegraph.com/subgraphs/name/decentraland/land-manager'
-      : 'https://api.thegraph.com/subgraphs/name/decentraland/land-manager-ropsten',
+      : 'https://api.thegraph.com/subgraphs/name/decentraland/land-manager-goerli',
     segmentKey:
       isStableVersion() && !isDevelopment()
         ? 'sFdziRVDJo0taOnGzTZwafEL9nLIANZ3'
