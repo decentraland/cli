7706d92ea125455a89e88d69b7e7007c4fd305e1
Lean Mendoza <leandro@decentraland.org>
2022-12-13 11:24:55 -0300

    feat: support workspace `dcl build` (#900)
    * feat: support workspace `dcl build`
    
    * add skip param
    
    * multiple scene errors
    
    * list project that fail
    
    Signed-off-by: Lean Mendoza <leandro@decentraland.org>

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -1,11 +1,12 @@
 import arg from 'arg'
 import chalk from 'chalk'
 
-import { buildTypescript } from '../utils/moduleHelpers'
-import { isTypescriptProject } from '../project/isTypescriptProject'
 import { fail } from 'assert'
-import { Analytics } from '../utils/analytics'
 import { Decentraland } from '../lib/Decentraland'
+import installDependencies from '../project/installDependencies'
+import updateBundleDependenciesField from '../project/updateBundleDependenciesField'
+import { Analytics } from '../utils/analytics'
+import { buildTypescript, isOnline } from '../utils/moduleHelpers'
 
 export const help = () => `
   Usage: ${chalk.bold('dcl build [options]')}
@@ -16,7 +17,8 @@ export const help = () => `
       -w, --watch               Watch for file changes and build on change
       -p, --production          Build without sourcemaps
       --skip-version-checks     Skip the ECS and CLI version checks, avoid the warning message and launch anyway
-
+      --skip-install            Skip installing dependencies
+      
     ${chalk.dim('Example:')}
 
     - Build your scene:
@@ -32,7 +34,8 @@ export async function main(): Promise<number> {
     '-w': '--watch',
     '--skip-version-checks': Boolean,
     '--production': Boolean,
-    '-p': '--production'
+    '-p': '--production',
+    '--skip-install': Boolean
   })
 
   const dcl = new Decentraland({
@@ -40,32 +43,76 @@ export async function main(): Promise<number> {
     workingDir: process.cwd()
   })
 
-  const workingDir = process.cwd()
   const skipVersionCheck = args['--skip-version-checks']
+  const skipInstall = args['--skip-install']
+  const online = await isOnline()
+  const errors = []
 
-  if (!dcl.workspace.isSingleProject()) {
-    fail(`Can not build a workspace. It isn't supported yet.`)
+  for (const project of dcl.workspace.getAllProjects()) {
+    const needDependencies = await project.needsDependencies()
+
+    if (needDependencies && !skipInstall) {
+      if (online) {
+        await installDependencies(
+          project.getProjectWorkingDir(),
+          false /* silent */
+        )
+      } else {
+        fail(
+          'This project can not start as you are offline and dependencies need to be installed.'
+        )
+      }
+    }
+
+    if (!skipVersionCheck) {
+      await project.checkCLIandECSCompatibility()
+    }
+
+    try {
+      await updateBundleDependenciesField({
+        workDir: project.getProjectWorkingDir()
+      })
+    } catch (err) {
+      console.warn(`Unable to update bundle dependencies field.`, err)
+    }
+
+    if (await project.isTypescriptProject()) {
+      try {
+        await buildTypescript({
+          workingDir: project.getProjectWorkingDir(),
+          watch: !!args['--watch'],
+          production: !!args['--production']
+        })
+      } catch (err) {
+        errors.push({ project, err })
+      }
+    }
   }
 
-  if (!skipVersionCheck) {
-    await dcl.workspace.getSingleProject()!.checkCLIandECSCompatibility()
+  if (errors.length) {
+    const projectList = errors
+      .map((item) => item.project.getProjectWorkingDir())
+      .join('\n\t')
+
+    throw new Error(
+      `Error compiling (see logs above) the scenes: \n\t${projectList}`
+    )
   }
 
-  if (await isTypescriptProject(workingDir)) {
-    await buildTypescript({
-      workingDir: workingDir,
-      watch: !!args['--watch'],
-      production: !!args['--production']
+  if (dcl.workspace.isSingleProject()) {
+    const baseCoords = await dcl.workspace.getBaseCoords()
+    Analytics.buildScene({
+      projectHash: dcl.getProjectHash(),
+      ecs: await dcl.workspace.getSingleProject()!.getEcsPackageVersion(),
+      coords: baseCoords,
+      isWorkspace: false
+    })
+  } else {
+    Analytics.buildScene({
+      projectHash: dcl.getProjectHash(),
+      isWorkspace: true
     })
   }
-
-  const baseCoords = await dcl.workspace.getBaseCoords()
-  Analytics.buildScene({
-    projectHash: dcl.getProjectHash(),
-    ecs: await dcl.workspace.getSingleProject()!.getEcsPackageVersion(),
-    coords: baseCoords,
-    isWorkspace: false
-  })
 
   return 0
 }
